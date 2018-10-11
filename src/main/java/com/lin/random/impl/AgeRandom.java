a356ad352c85cde70f998f2ff119681346ac8ca0
lkmc2 <lkmc2@163.com>
2018-10-11 11:50:10 +0800

    简化随机年龄生成器的逻辑

--- src/main/java/com/lin/random/impl/AgeRandom.java
+++ src/main/java/com/lin/random/impl/AgeRandom.java
@@ -1,24 +1,18 @@
 package com.lin.random.impl;
 
-import com.lin.random.base.BaseRandomCreator;
+import com.lin.random.RandomData;
+import com.lin.utils.RandomUtils;
 
 /**
  * 随机年龄生成器（18到60岁）
  * @author lkmc2
  * @since 1.0.0
  */
-public class AgeRandom extends BaseRandomCreator<Integer> {
+public class AgeRandom implements RandomData<Integer> {
 
     @Override
-    public Integer[] createOptionsArray() {
-        Integer[] ages = new Integer[43];
-
-        // 设置年龄范围为18到60岁
-        for (int i = 18; i <= 60; i++) {
-            ages[i - 18] = i;
-        }
-
-        return ages;
+    public Integer next() {
+        return RandomUtils.nextIntRange(18, 60);
     }
 
 }
