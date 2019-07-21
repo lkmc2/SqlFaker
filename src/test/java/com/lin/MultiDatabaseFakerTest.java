b04fa258309f3f964ae33748acd52cc3d1355734
lkmc2 <lkmc2@163.com>
2019-07-21 09:27:01 +0800

    添加Sqlite的Faker测试用例

--- src/test/java/com/lin/MultiDatabaseFakerTest.java
+++ src/test/java/com/lin/MultiDatabaseFakerTest.java
@@ -119,4 +119,39 @@ public class MultiDatabaseFakerTest {
                 .execute();
     }
 
+    @Test
+    public void testSqlite() {
+        // 创建 Sqlite 数据库连接
+        DBTools.url("jdbc:sqlite:F:/SqliteWorkplace/my_sqlite.db")
+                .driverClassName("org.sqlite.JDBC")
+                .username("")
+                .password("")
+                .connect();
+
+        // 给 user_table 表的 3 个字段填充 5 条数据
+        Faker.tableName("user_table")
+                .param("id", DataType.ID)
+                .param("name", DataType.USERNAME)
+                .param("age", DataType.AGE)
+                .insertCount(5)
+                .execute();
+
+        // 给 product 表的 9 个字段填充 5 条数据
+        Faker.tableName("product")
+                .param("id", DataType.ID)
+                .param("name", DataType.USERNAME)
+                .param("price", Values.ofIntRange(10, 100))
+                .param("tenant_id  ", DataType.ID)
+                .param("created_by", DataType.USERNAME)
+                .param("updated_by", DataType.USERNAME)
+                .param("created_at", Values.ofTimeRange(
+                        Times.of(2018, 7, 12),
+                        Times.of(2019, 9, 22, 18, 22, 33)
+                ))
+                .param("updated_at", DataType.TIME)
+                .param("dr", Values.of("0", "1"))
+                .insertCount(5)
+                .execute();
+    }
+
 }
